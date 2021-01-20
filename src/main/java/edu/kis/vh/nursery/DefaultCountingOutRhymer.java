174b42071293b0955446d298500fa9fe21e88e25
Michal Gladysz <michal.gladysz@hotmail.com>
2021-01-20 13:48:37 +0100

    Zad 1.4.3 dokonanie hermetyzacji nieprywatnych atrybutow, usuniecie nieuzywanych setterow

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -3,32 +3,34 @@ package edu.kis.vh.nursery;
 public class DefaultCountingOutRhymer {
 
   private int[] numbers = new int[12];
-
-  public int total = -1;
+  private int total = -1;
 
   public void countIn(int in) {
     if (!isFull())
-      numbers[++total] = in;
+      numbers[total = getTotal() + 1] = in;
   }
 
   public boolean callCheck() {
-    return total == -1;
+    return getTotal() == -1;
   }
 
   public boolean isFull() {
-    return total == 11;
+    return getTotal() == 11;
   }
 
   protected int peekaboo() {
     if (callCheck())
       return -1;
-    return numbers[total];
+    return numbers[getTotal()];
   }
 
   public int countOut() {
     if (callCheck())
       return -1;
-    return numbers[total--];
+    return numbers[total = getTotal() - 1];
   }
 
+  public int getTotal() {
+    return total;
+  }
 }
